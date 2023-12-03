ce2f254b40710579ab112a9df44ce6b72c2650b2
Mac McGee <macmcgee@Macs-Air.attlocal.net>
2023-12-03 16:40:51 -0600

    project

--- index.js
+++ index.js
@@ -1,100 +1,74 @@
+// having trouble getting the buttons to actually function for adding or deleting parties. 
+const events = [];
 const API_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-GHP-ET-WEB-FT-SF/events';
 const state = { parties: [] };
-
 const partyListItems = document.getElementById('party-list-items');
 const addPartyForm = document.getElementById('add-party-form');
+const deleteButtons = document.querySelectorAll('.delete-button');
 
-// Fetch parties on page load
-fetchParties();
-
-// Add party form submit event listener
-addPartyForm.addEventListener('submit', addParty);
-
-// Function to fetch parties from the API
-function fetchParties() {
-  fetch(API_URL)
-    .then(response => response.json())
-    .then(data => {
-      const parties = data.parties;
-      state.parties = data;
-      renderParties();
-    })
-    
+// Function to fetch events from API
+async function fetchEvents() {
+    const response = await fetch('API_URL');
+    const data = await response.json();
+    events.push(data);
 }
 
-// Function to add a new party
-function addParty(event) {
-  event.preventDefault();
+// Function to render events
+function renderEvents() {
+    const eventsList = document.getElementById('events-list');
+    eventsList.innerHTML = '';
 
-  const partyData = {
-    name: document.getElementById('party-name').value,
-    date: document.getElementById('party-date').value,
-    time: document.getElementById('party-time').value,
-    location: document.getElementById('party-location').value,
-    description: document.getElementById('party-description').value
-  };
+    for (const event of events) {
+        const eventItem = document.createElement('li');
+        eventItem.innerHTML = `
+            <h2>${event.name}</h2>
+            <p>
+                <strong>Date:</strong> ${event.date}<br>
+                <strong>Time:</strong> ${event.time}<br>
+                <strong>Location:</strong> ${event.location}<br>
+                <strong>Description:</strong> ${event.description || ''}
+            </p>
+            <button class="delete-button" data-id="${event.id}">Delete</button>
+        `;
 
-  // Send a POST request to the API to create a new party
-  fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(partyData)
-  })
-    .then(response => response.json())
-    .then(() => {
-      fetchParties(); // Refresh the party list
-      clearForm(); // Clear the form fields
-    })
-    .catch(error => console.error('Error adding party:', error));
-}
+        const button = eventItem.querySelector('.delete-button');
+        button.addEventListener('click', async () => {
+            const eventId = button.dataset.id;
 
-// Function to delete a party
-function deleteParty(eventId) {
-  // Send a DELETE request to the API to remove the specified party
-  fetch(`<span class="math-inline">\{API\_URL\}/</span>{eventId}`, {
-    method: 'DELETE'
-  })
-    .then(response => response.json())
-    .then(() => fetchParties()) // Refresh the party list
-    .catch(error => console.error('Error deleting party:', error));
-}
+            // Send delete request to API
+            await fetch(`${API_URL}/${eventId}`, { method: 'DELETE' });
 
-// Function to render the list of parties
-function renderParties() {
-  fetch(API_URL)
-  .then(response => response.json())
-  .then(data => {
-    const parties = data.parties; // Extract the array of parties from the response data
-    state.parties = parties;
-    renderParties();
-  })
-  .catch(error => console.error('Error fetching parties:', error));
-}
+            // Remove event from local storage
+            events.splice(events.findIndex(event => event.id === eventId), 1);
 
-function renderParties() {
-  partyListItems.textContent = ''; // Clear existing content
-}
-  if (state.parties.length === 0) {
-    // Display a message if no parties are found
-    const noPartiesMessage = document.createElement('p');
-    noPartiesMessage.textContent = 'No parties found.';
-    partyListItems.appendChild(noPartiesMessage);
-  } else {
-    // Create and append a DOM element for each party
-    for (const party of state.parties) {
-      const partyElement = createPartyElement(party); // Pass the party object to createPartyElement
-      partyListItems.appendChild(partyElement);
+            // Render events again
+            renderEvents();
+        });
+
+        eventsList.appendChild(eventItem);
     }
-  }
- if (state.parties && Array.isArray(state.parties)) {
-    // If state.parties is an array, iterate over it
-    for (const party of state.parties) {
-      const partyElement = createPartyElement(party);
-      partyListItems.appendChild(partyElement);
-    }
-  } else {
-    // If state.parties is not an array, handle the error
-    console.error('Error: state.parties is not an iterable object');
-  }
+}
+
+// Function to handle add event form submission: not sure why or how to get the add buttton to function 
+addPartyForm.addEventListener('submit', async (event) => {
+   
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    // Send create request to API
+    await fetch('API_URL', {
+        method: 'POST',
+        body: JSON.stringify(Object.fromEntries(formData))
+    });
+
+    // Clear form
+    event.target.reset();
+
+    // Fetch and render events again
+    await fetchEvents();
+    renderEvents();
+});
+
+// Initial fetch and render
+fetchEvents().then(renderEvents);
